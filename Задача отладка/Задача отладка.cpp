553e9581fa3e001b71f1b994fff52b5d4329a1fe
kosst <kosst@DESKTOP-14TLCGC>
2023-02-21 17:30:16 +0300

    Добавьте файлы проекта.

---
+++ Задача отладка/Задача отладка.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+class A 
+{
+public:
+	virtual const char getName() { return 'A'; }
+};
+class B :public A
+{
+public:
+	virtual const char getName() { return 'B'; }
+};
+int main()
+{
+	B* b = new B();
+	A* a = b;
+	cout << "Name of class " << a->getName() << endl;
+
+}
